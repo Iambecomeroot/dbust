82e7e2f0fde27791d722ba6466eac45a770d1ab7
Marcel Robitaille <marcelrobitaille11@gmail.com>
2017-01-17 11:28:57 -0400

    I should write integration tests

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict'
 
 const path = require('path')
+const load = (file) => require(path.join(__dirname, file))
 
 const fs = require('pn/fs')
 
-const dbust = require(path.join(__dirname, './dbust.js'))({ fs })
+const locker = load('locker.js')(fs)
+const dbust = load('dbust.js')({ fs, locker })
 
 module.exports = dbust
